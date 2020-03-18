ff02cacf7b5fed76f8de310ea6f5ea0d501885e4
junglespaghetti <61948148+junglespaghetti@users.noreply.github.com>
2020-03-19 07:13:49 +0900

    Create trade_recorder.js

---
+++ trade_recorder.js
@@ -0,0 +1,32 @@
+const jsFrame = new JSFrame();
+//Create window
+const frame01 = jsFrame
+  .create({
+    title: "Yosemite style",
+    left: 20,
+    top: 20,
+    width: 320,
+    height: 220,
+    appearanceName: "yosemite", //Now preset is selectable from  'yosemite','redstone','popup'
+    style: {
+      backgroundColor: "rgba(255,255,255,0.9)"
+    },
+    html: '<div id="jsframe-html-top"></div>'
+  })
+  .show();
+var db = new Dexie("MyDatabase");
+db.version(1).stores({
+  friends: "++id, name, age, *tags",
+  gameSessions: "id, score"
+});
+//  db.open();
+
+db.friends.add({ name: "test", age: "test note" });
+
+var opt = {
+  delimiter: "<>"
+};
+
+db.friends.toArray().then(function(data) {
+  alert(Papa.unparse(data, opt));
+});
