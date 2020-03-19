2d4beaef9aeccd0c51e56813f143fdba7ee00c4e
junglespaghetti <junglecircus13@gmail.com>
2020-03-19 05:47:05 +0000

    move

---
+++ js/trade-recorder-bookmarklet.js
@@ -0,0 +1,20 @@
+javascript: (function(f, urls, i, s) {
+  urls = [
+    "https://junglespaghetti.github.io/trade-recorder/trade_recoder.js",
+    "https://cdn.jsdelivr.net/npm/jsframe.js@1.5.16/lib/jsframe.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/dexie/2.0.4/dexie.min.js"
+  ];
+  for (i = 0; i < urls.length; i++) {
+    s = document.createElement("script");
+    s.src = urls[i];
+    if (i == urls.length - 1) {
+      s.onload = function() {
+        f();
+      };
+    }
+    document.body.appendChild(s);
+  }
+})(function() {
+  startMain();
+});
