b9ef97d93617e2b44346493925ae5e248aa90191
junglespaghetti <junglecircus13@gmail.com>
2020-03-19 05:55:57 +0000

    cng urls

--- js/trade-recorder-bookmarklet.js
+++ js/trade-recorder-bookmarklet.js
@@ -1,6 +1,6 @@
 javascript: (function(f, urls, i, s) {
   urls = [
-    "https://junglespaghetti.github.io/trade-recorder/trade_recoder.js",
+    "https://junglespaghetti.github.io/trade-recorder/js/trade-recoder.js",
     "https://cdn.jsdelivr.net/npm/jsframe.js@1.5.16/lib/jsframe.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/dexie/2.0.4/dexie.min.js"
