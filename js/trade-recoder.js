6056273045ed876d55789dc50892326dc3eea029
junglespaghetti <junglecircus13@gmail.com>
2020-03-19 08:35:19 +0000

    add sjpanel style

--- js/trade-recoder.js
+++ js/trade-recoder.js
@@ -22,13 +22,13 @@ db.friends.toArray().then(function(data) {
 
 function createFrame(){
 jsPanel.create({
-    theme:       'primary',
+    theme:       'primary filleddark',
     headerTitle: 'my panel #1',
     position:    'center-top 0 58',
     contentSize: '450 250',
     content:     '<p>Example panel ...</p>',
     callback: function () {
-        this.content.style.padding = '20px';
+        this.content.style.borderRadius = '.33rem';
     },
     onbeforeclose: function () {
         return confirm('Do you really want to close the panel?');
