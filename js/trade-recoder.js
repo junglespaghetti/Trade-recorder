b0d0e348d88a0a61a08050aadb085d466ba8eb58
junglespaghetti <junglecircus13@gmail.com>
2020-03-21 20:53:06 +0000

    cng

--- js/trade-recoder.js
+++ js/trade-recoder.js
@@ -2,12 +2,12 @@ function startMain(){
     var htmlContents = '<div id="easyIndexedDB-contents" style="margin:8px;>\
     <ul id="easyIndexedDB-pages" style="list-style: none;">\
         <li id="easyIndexedDb-edit-form">\
-            <h3>Easy IndexedDB</h3>\
+            <h3 style="line-height:0.5;>Easy IndexedDB</h3>\
             <p style="line-height:0.5;">DB Name : <input id="easyindexedDB-DB-name"></p>\
             <p style="line-height:0.5;">Version:</p>\
             <p style="line-height:0.5;">Origin url:</p>\
             <p style="line-height:0.5;"><button id="easyIndexedDB-add-button" type="button">Add Table</button> Table name: Columns(Comma separated)</p>\
-            <ol id="easyIndexedDB-table-list" style="padding-left: 10px;line-height:1.5;">\
+            <ol id="easyIndexedDB-table-list" style="padding-left: 10px;">\
                 <li>\
                     <input> : <input><span><i class="fas fa-times"></i></span>\
                 </li>\
