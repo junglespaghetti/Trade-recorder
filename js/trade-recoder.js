cac6523ac877821acfa7a7b5f1322e56986bb161
junglespaghetti <junglecircus13@gmail.com>
2020-03-21 03:25:45 +0000

    cng

--- js/trade-recoder.js
+++ js/trade-recoder.js
@@ -98,7 +98,7 @@ eDB.dbList.toArray().then(function(data) {
 });
 });
 let dataInput = document.getElementById("db-input");
-    dataInput.addEventListener('chnge', selestDB(), false);
+    dataInput.addEventListener('change', selestDB(), false);
     this.headertoolbar.querySelectorAll('span').forEach(function(item) {
       item.style.cursor = 'pointer';
       item.style.marginRight = '4px';
