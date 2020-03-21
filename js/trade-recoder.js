c7f356cee054c506c8974e4403217366b45c16b9
junglespaghetti <junglecircus13@gmail.com>
2020-03-21 02:27:29 +0000

    cng

--- js/trade-recoder.js
+++ js/trade-recoder.js
@@ -75,11 +75,14 @@ function createFrame(){
   callback: function (panel) {
       Dexie.exists("easyIndexedDB").then(function(exists){
     var eDB = new Dexie("easyIndexedDB");
-    
+    if(!exists){
     eDB.version(1).stores({
         dbList: "++id, name, version, table ",
         settings: "name, value"
         });
+    }else{
+        eDB.version(1);
+    }
     alert("easyIndexedDB");   
         eDB.dbList.put({name:"easyIndexedDB",version:1,table:["dbList","settings"]});
         eDB.settings.put({name:"status",value:"new"});
