306196e40eafbd64beb3a489689a062c6299cadb
junglespaghetti <junglecircus13@gmail.com>
2020-03-21 04:47:47 +0000

    cng

--- js/trade-recoder.js
+++ js/trade-recoder.js
@@ -1,7 +1,67 @@
-const easyIndexedDB = "easyIndexedDB"
-
 function startMain(){
-createFrame();
+    var easyIndexedDB = jsPanel.create({
+        headerTitle: 'host:' + location.hostname,
+        position:    'center-top 0 58',
+        contentSize: '450 250',
+        headerLogo:'<input type="text" id="db-input" list="db-list" placeholder="input DB name" autocomplete="off" style="margin-left:8px;font-size:8pt;"><datalist id="db-list"><option value="this db"></datalist>',
+        headerToolbar: '<input type="text" id="table-input" list="table-list" placeholder="input Table name" autocomplete="off" style="font-size:8pt;"><datalist id="table-list"><option value="table"></datalist>' +
+                    '<div style="margin-left:8px;">' +               
+                    '<span id="bus"><i class="fas fa-file-import fa-lg"></i></i></span>'+
+                    '<span id="train"><i class="fas fa-file-download fa-lg"></i></span>'+
+                    '<span id="car"><i class="fas fa-hand-holding-usd fa-lg"></i></span>'+
+                    '<span id="car"><i class="fas fa-money-check-alt fa-lg"></i></span>'+
+                    '<span id="car"><i class="fas fa-chart-line fa-lg"></i></span>'+
+                    '<span id="bicycle"><i class="fas fa-calculator fa-lg"></i></span>' +
+                    '</div>',
+        callback: function (panel) {
+            Dexie.exists("easyIndexedDB").then(function(exists){
+                var eDB = createEasyIndexedDB();
+                if(!exists){
+                    eDB.dbList.put({name:"easyIndexedDB",version:1,table:["dbList","settings"]});
+                    eDB.settings.put({name:"status",value:"new"});
+                }
+                eDB.dbList.toArray().then(function(data) {
+                    let dataList = document.getElementById("db-list");
+                    data.forEach(function(val){
+                        let option = document.createElement("option");
+                        option.text = val.name;
+                        option.value = val.name;
+                        dataList.appendChild(option);
+                    })
+                });
+            });
+            let dbInput = document.getElementById("db-input");
+            dbInput.addEventListener('change',function (event){selectDB(event)});
+            let tableInput = document.getElementById("table-input");
+            tableInput.addEventListener('change',function (event){selectTable(event)});
+                this.headertoolbar.querySelectorAll('span').forEach(function(item) {
+                    item.style.cursor = 'pointer';
+                    item.style.marginRight = '4px';
+                    item.addEventListener('click', function() {
+                    panel.content.innerHTML = 'You clicked the ' + item.id + ' icon!';
+                }); 
+            });
+        }
+    });
+}
+function createEasyIndexedDB(){
+    var eDB = new Dexie("easyIndexedDB");
+    eDB.version(1).stores({
+        dbList: "++id, name, version, table ",
+        settings: "name, value"
+    });
+    return eDB
+}
+
+
+function selectDB(event){
+    if(!event.target.value){
+
+    }
+}
+
+function selectTable(event){
+
 }
 
 async function getRecorderDB(){
@@ -106,8 +166,4 @@ let dataInput = document.getElementById("db-input");
     });
   }
 });
-}
-
-function selectDB(event){
-    alert("aaaa");
 }
