9c0f0cc2e888263de0f5d72f7e12157fec8235b9
junglespaghetti <junglecircus13@gmail.com>
2020-03-22 04:52:04 +0000

    cng

--- js/trade-recoder.js
+++ js/trade-recoder.js
@@ -98,7 +98,7 @@ function addTableListLi(){
     span.innerHTML = '<i class="fas fa-times"></i>';
     span.class = 'easyIndexedDB-tablelist-delete';
     span.addEventListener('click',function (event){
-        removeElem.parentNode.removeChild(event.target);
+        removeElem.parentNode.removeChild(event.getSource().getElement());
         let parent = event.target.parent;
     });
     let tableOl = document.getElementById("easyIndexedDB-table-list");
